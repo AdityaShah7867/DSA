3bf4373dc5bb20a31f4c9e3017706ed83857fef0
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-08-02 08:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (17.17%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,21 +1,29 @@
-#include <climits>
-
 class Solution {
 public:
     int reverse(int x) {
-        int ans = 0;
+        int rev = 0;
+        int sign = (x < 0) ? -1 : 1; // Determine the sign
+        x = abs(x); // Work with the absolute value of x
 
-        while (x != 0) {
-            int digit = x % 10;
-
-            if (ans > INT_MAX / 10 || ans < INT_MIN / 10) {
-                return 0;
+        while (x > 0) {
+            int ld = x % 10; // Get the last digit
+            
+            // Check for overflow before updating rev
+            if (rev > (INT_MAX - ld) / 10) {
+                return 0; // Return 0 if overflow would occur
             }
 
-            ans = (ans * 10) + digit;
-            x = x / 10;
+            rev = rev * 10 + ld;
+            x = x / 10; // Remove the last digit from x
         }
 
-        return ans;
+        rev *= sign; // Reapply the sign to the reversed number
+
+        // Check if the final reversed number is within 32-bit signed integer range
+        if (rev < INT_MIN || rev > INT_MAX) {
+            return 0; // Return 0 if out of bounds
+        }
+
+        return rev;
     }
 };
