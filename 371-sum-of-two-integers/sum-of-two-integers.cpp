621054c46d809879d996dbe385a5e1b9d6f54162
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-09-17 23:17:09 +0530

    Time: 2 ms (56.69%) | Memory: 7.4 MB (24.39%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while (b != 0) {
+            int tmp = (a & b) << 1; // carry calculation
+            a = a ^ b; // sum without carry
+            b = tmp; // carry shifted left
+        }
+        return a;
     }
 };
