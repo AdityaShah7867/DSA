dd00c4970e62b463f382fe2a0ba773445fa38538
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-10-04 01:20:43 +0530

    Time: 14 ms (14.99%) | Memory: 17.2 MB (20.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,22 +5,23 @@ public:
         int m = matrix[0].size();
         vector<bool> rows(n,false);
         vector<bool> cols(m,false);
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < m ; j++) {
-                if(matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    rows[i]=true;
+                    cols[j]=true;
                 }
             }
         }
-        for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < m ; j++) {
-                if(rows[i] || cols[j]) {
-                      matrix[i][j] = 0;
+
+          for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(rows[i] || cols[j]){
+                    matrix[i][j]=0;
                 }
             }
-
-
+        }
     }
-}
 };
