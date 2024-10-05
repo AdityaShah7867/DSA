110181c9d2186f8ec8ddc204f8570115b8a48831
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-10-05 09:16:19 +0530

    Time: 30 ms (29.96%) | Memory: 30.2 MB (73.37%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end =  nums.size() - 1;
-        int mid = (start+end)/2;
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
 
-        while(start <= end){
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target)return mid;
 
-            if(nums[mid] == target){
-                return mid;
-            }
-
-            if(target > nums[mid]){
-                start = mid + 1;
+            if(nums[mid]>target){
+                high=mid-1;
             }else{
-                end = mid -1;
+                low=mid+1;
             }
-
-            mid = (start + end) /2 ;
         }
-
         return -1;
     }
 };
