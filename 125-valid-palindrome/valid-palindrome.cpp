65de3d857fbb1928bfdd4035f199a94b8aa02e0b
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-10-05 11:38:45 +0530

    Time: 7 ms (36.83%) | Memory: 9.7 MB (12.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,52 @@
 class Solution {
+
+private:
+    bool isValid(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+            (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    char toLower(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+            return ch;
+        } else {
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+
+    bool palindrome(string s) {
+        int st = 0;
+        int e = s.length() - 1;
+
+        while (st < e) {
+            if (s[st] != s[e]) {
+                return false;
+            } else {
+                st++;
+                e--;
+            }
+        }
+        return true;
+    }
+
 public:
     bool isPalindrome(string s) {
-        string proper;
-        if (s.size() <= 1) return true;
 
-        for (char c : s) {
-            if (isalnum(c)) {
-                proper += tolower(c); 
+        string temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (isValid(s[i])) {
+                temp.push_back(s[i]);
             }
         }
 
-    
-        int n = proper.size();
-        for (int i = 0; i < n / 2; i++) {
-            if (proper[i] != proper[n - 1 - i]) {
-                return false; 
-            }
+        for (int i = 0; i < temp.length(); i++) {
+            temp[i] = toLower(temp[i]);
         }
-        
-        return true; 
+
+        return palindrome(temp);
     }
 };
