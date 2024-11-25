d5ec97bcf7044b199268f3653e553af2f7fc15ea
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-11-25 06:39:04 +0530

    Time: 7 ms (85.00%) | Memory: 10.7 MB (96.37%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -10,24 +10,20 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
 
-        if(head == NULL || head->next == NULL){
-            return false;
-        }
-
-        ListNode* slow=head;
-        ListNode* fast = head;
-
-
-        while(fast->next && fast->next->next){
-            fast = fast->next->next;
-            slow = slow->next;
-
-            if(fast == slow){
-                return true;
-            }
-        }
-
+      if(head==NULL || head->next == NULL){
         return false;
-        
+      }
+      ListNode* fast =head;
+      ListNode* slow =head;
+
+      while(fast->next && fast->next->next){
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if(fast == slow){
+            return true;
+        }
+      }
+    return false;
     }
 };
