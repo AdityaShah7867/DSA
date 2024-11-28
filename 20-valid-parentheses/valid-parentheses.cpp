59335b14671feafffe74e70b131e33bcefefded1
ADITYA NIKUNJ SHAH <192105adityashah@gmail.com>
2024-11-28 16:29:23 +0530

    Time: 2 ms (14.86%) | Memory: 8.9 MB (10.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack; // initialize new empty stack;
+        stack<char> stack;
 
-        for (int i = 0; i < s.size(); i++) {
-
+        for (int i=0; i<s.size(); i++){
             if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
                 stack.push(s[i]);
-            } else {
-                if (stack.empty()) {
-                    return false; // if we have closing bracket but no opening bracket in stack
-                } else {
-                    char t = stack.top();
-
-                    if (s[i] == ')' && t != '(' || s[i] == ']' && t != '[' ||
-                        s[i] == '}' && t != '{' ) {
+            }
+            else{
+                if(stack.empty()){
+                    return false;
+                }
+                else{
+                    char topp = stack.top();
+                    if(s[i] == ')' && topp != '(' || s[i] == ']' && topp != '[' || s[i] == '}' && topp != '{' ){
                         return false;
                     }
-                    else{
-                        stack.pop();
-                    }
+                    stack.pop();
                 }
             }
         }
         return stack.empty();
+        
     }
 };
